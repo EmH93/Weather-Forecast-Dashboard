24ee55faedfb79c32aa49451ca006653c2949998
Emma Hayward <emmahayward93@gmail.com>
2023-01-26 19:42:52 +0000

    Turned push to html and query api into a function so it can be used for the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,10 +28,16 @@
         weatherToday.empty();
         forecastDiv.empty();
 
+        userSearch(userInput);
+    })
+
+
+//function for query to API and pushing to screen.
+    function userSearch (userInput){
     //if else statement to validate user input - no empty searches allowed
-    if(userInput === ""){
-        alert("Enter a place name, then click search to continue.");
-    } else { 
+        if(userInput === ""){
+            alert("Enter a place name, then click search to continue.");
+        } else { 
 
     //Push user input to search history in local storage
         searchHistory.push(userInput);
@@ -47,40 +53,31 @@
                 url: queryURL,
                 method: "GET"
             }).then(function(response){
-                console.log(response)
-            //Main weather report
-                
-            //Tomorrow weather
                 let listArr = response.list;
                 for ( var i = 0; i < listArr.length; i++){
                     if(listArr[i] === listArr[0]){
                         weatherToday.attr("id", "todayForecast");
-                var cityName = $("<h1>").text(response.city.name);
-                var iconcode = response.list[0].weather[0].icon;
-                var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-                var weatherIcon = $("<img>").attr("src", iconurl);
-                var cityTemp = $("<h4>").text("Temp: "+ response.list[0].main.temp + "°C");
-                var cityWind = $("<h4>").text("Wind: " + response.list[0].wind.speed + " kph");
-                var cityHumidity = $("<h4>").text("Humidity: " + response.list[0].main.humidity + "%");
-                weatherToday.append(cityName, todayDate, weatherIcon, cityTemp, cityWind, cityHumidity);
+                        var cityName = $("<h1>").text(response.city.name);
+                        var iconcode = response.list[0].weather[0].icon;
+                        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+                        var weatherIcon = $("<img>").attr("src", iconurl);
+                        var cityTemp = $("<h4>").text("Temp: "+ response.list[0].main.temp + "°C");
+                        var cityWind = $("<h4>").text("Wind: " + response.list[0].wind.speed + " kph");
+                        var cityHumidity = $("<h4>").text("Humidity: " + response.list[0].main.humidity + "%");
+                        weatherToday.append(cityName, todayDate, weatherIcon, cityTemp, cityWind, cityHumidity);
                     } else {
-                    let weatherDiv = $("<div>").attr("id", "futureForecast");
-                    weatherDiv.attr("data-number", [i]);
-                    var cityNameSml = $("<h4>").text(response.city.name);
-                    var iconcodeSml = response.list[i].weather[0].icon;
-                    var iconurlSml = "http://openweathermap.org/img/w/" + iconcodeSml + ".png";
-                    var weatherIconSml = $("<img>").attr("src", iconurlSml);
-                    var cityTempSml = $("<h4>").text("Temp: "+ response.list[i].main.temp + "°C");
-                    var cityWindSml = $("<h4>").text("Wind: " + response.list[i].wind.speed + " kph");
-                    var cityHumiditySml = $("<h4>").text("Humidity: " + response.list[i].main.humidity + "%");
-                    weatherDiv.append(cityNameSml, weatherIconSml, cityTempSml, cityWindSml, cityHumiditySml);
-                    forecastDiv.append(weatherDiv);
-                    }
-                }
-                
-            
+                        let weatherDiv = $("<div>").attr("id", "futureForecast");
+                        weatherDiv.attr("data-number", [i]);
+                        var cityNameSml = $("<h4>").text(response.city.name);
+                        var iconcodeSml = response.list[i].weather[0].icon;
+                        var iconurlSml = "http://openweathermap.org/img/w/" + iconcodeSml + ".png";
+                        var weatherIconSml = $("<img>").attr("src", iconurlSml);
+                        var cityTempSml = $("<h4>").text("Temp: "+ response.list[i].main.temp + "°C");
+                        var cityWindSml = $("<h4>").text("Wind: " + response.list[i].wind.speed + " kph");
+                        var cityHumiditySml = $("<h4>").text("Humidity: " + response.list[i].main.humidity + "%");
+                        weatherDiv.append(cityNameSml, weatherIconSml, cityTempSml, cityWindSml, cityHumiditySml);
+                        forecastDiv.append(weatherDiv);
+                    }} 
             })
         }
-    })
-
-    
+    }
